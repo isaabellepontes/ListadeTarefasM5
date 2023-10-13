eb129d6212d9750470270f35170c4bcd99936152
isaabellepontes <isaapontes010@gmail.com>
2023-10-12 22:48:13 -0300

    Feat: Criação do Botao de editar e colocação do onClick

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Todo = ({ todo, removeTodo, completeTodo }) => {
+const Todo = ({ todo, removeTodo, completeTodo, setEditingTodo }) => {
     return (
-        <div className='todo' style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}>
-                    <div className='content'>
-                        <p>{todo.text}</p>
-                        <p className='categoria'>({todo.categoria})</p>
-                    </div>
-                    <div>
-                        <button className='Complete' onClick={() => completeTodo(todo.id)}> Completar </button>
-                        <button className='Remove' onClick={() => removeTodo(todo.id)}>X</button>
-                    </div>
+        <div className='todo' style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
+            <div className='content'>
+                <p>{todo.text}</p>
+                <p className='categoria'>({todo.categoria})</p>
+            </div>
+            <div>
+                <button className='Complete' onClick={() => completeTodo(todo.id)}>Completar</button>
+                <button className='Remove' onClick={() => removeTodo(todo.id)}>X</button>
+                <button className='Edit' onClick={() => setEditingTodo(todo)}>Editar</button>
+            </div>
         </div>
     );
 };
